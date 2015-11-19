46b1d592f13a20083790bdf55cf76c3ca6101d2d
Steve <steve@Steves-MacBook-Pro.local>
2015-11-19 15:51:13 -0500

    finished forms. edited for serialized data. fixed syntax error

--- public/script.js
+++ public/script.js
@@ -6,62 +6,80 @@ $(function(){
 
 
 
-    $('#showArtist').click( =>(e){
+    $('#showArtist').click( (e) => {
       e.preventDefault();
 
     $.ajax({
-      url: "/artists"
-    }).done( => (data){
+       url: "/artists"
+    }).done(  (data) => {
       console.log('show artist')
       showArtist(data);
     });
   });
 
-    let showArtist = =>(data){
+    let showArtist = (data) => {
       let $results = $('.results-div');
       let compiledTemplate = renderTemplate_show_artists(data);
       $results.html('').append(compiledTemplate)
     }
-  }
+
 
 
 //pulls new artist info from front end and sends to server.
-  $('#artist_form_submit').click( => (e){
+  $('#artist_form_submit').click( (e) =>  {
     e.preventDefault();
-    saveArtistData = {};
-    saveArtistData.name = $('#name').val();
-    saveArtistData.img_url = $('#img_url').val();
-    saveArtistData.nationality = $('#nationality').val();
-    saveArtistData.birthYear = $('#birthYear').val();
-    saveArtistData.description = $('#description').val();
-    console.log(data)
+    // saveArtistData = {};
+    // saveArtistData.name = $('#name').val();
+    // saveArtistData.img_url = $('#img_url').val();
+    // saveArtistData.nationality = $('#nationality').val();
+    // saveArtistData.birthYear = $('#birthYear').val();
+    // saveArtistData.description = $('#description').val();
+    // console.log(data)
 
+    var new_artist_data = $('.artist_form').serialize();
     $.ajax({
       type: "POST",
       url: "/artists",
-      data: saveArtistData;
+      data: new_artist_data
       console.log('artist has been saved')
     })
   })
 
 
   //edit artist info from front end and sends to server.
-    $('#artist_edit_submit').click( => (e){
+    $('#artist_edit_submit').click( (e) => {
       e.preventDefault();
-      editArtistData = {};
-      editArtistData.name = $('#name').val();
-      editArtistData.img_url = $('#img_url').val();
-      editArtistData.nationality = $('#nationality').val();
-      editArtistData.birthYear = $('#birthYear').val();
-      editArtistData.description = $('#description').val();
-      console.log(editArtistData);
+      // editArtistData = {};
+      // editArtistData.name = $('#name').val();
+      // editArtistData.img_url = $('#img_url').val();
+      // editArtistData.nationality = $('#nationality').val();
+      // editArtistData.birthYear = $('#birthYear').val();
+      // editArtistData.description = $('#description').val();
+      // console.log(editArtistData);
+      var edit_artist_data = $('.artist_edit_form').serialize();
 
       $.ajax({
         type: "PUT",
         url: "/artists",
-        data: editArtistData;
+        data: edit_artist_data
         console.log('artist has been saved')
       })
     })
 
+  //edit painting info
+    $('#painting_form_submit').click( (e) => {
+      e.preventDefault();
+      // editPaintingData = {};
+      // editPaintingData.title = $('#title').val();
+      // editPaintingData.img_url = $('#img_url').val();
+      // editPaintingData.year_made = $('#year_made').val();
+      // console.log(editPaintingData);
+        var edit_painting_data = $('.painting_form').serialize();
+      $.ajax({
+        type: "PUT",
+        url: "/paintings",
+        data: edit_painting_data;
+        console.log('painting data updated')
+      })
+    })
 });
