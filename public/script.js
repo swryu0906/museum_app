d13135bee1b4020f5d5eff133939609f9162570b
General Assembly | Anthony Ng <generalassemblyanthonyng@Anthonys-MacBook-Air.local>
2015-11-19 15:57:04 -0500

    Update click events for the links in HTML
    . in script.js, add click events for each of the four links from HTML
    . in script.js, create a utility function to clear away old view
    . fixed syntax errors with "=>" notation

--- public/script.js
+++ public/script.js
@@ -2,26 +2,106 @@
 
 $(function(){
 
-  let renderTemplate_show_artists = Handlebars.compile($('template#NAME').html());
+  let renderTemplate_show_artists = Handlebars.compile($('template#artist-template').html());
+  let renderTemplate_show_paintings = Handlebars.compile($('template#painting-template').html());
+  let renderTemplate_show_artists_form = Handlebars.compile($('template#artist-form-template').html());
+  let renderTemplate_show_paintings_form = Handlebars.compile($('template#painting-form-template').html());
 
-    $('#showArtist').click( =>(e){
-      e.preventDefault();
+// =================================================================
+// Link Click Events ===============================================
+// =================================================================
+
+  $('#showArtist').click((e) => {
+    e.preventDefault();
 
     $.ajax({
-      // need URL from SUNGWON
-      url: ""
-    }).done( => (data){
-      console.log('show artist')
+      type: "GET",
+      url: "/artists"
+    }).done((data) => {
+      console.log('show artists');
       showArtist(data);
     });
   });
 
-    let showArtist = =>(data){
-      let $results = $('.results-div');
-      let compiledTemplate = renderTemplate_show_artists(data);
-      $results.html('').append(compiledTemplate)
-    }
+  $('#showPainting').click((e) => {
+    e.preventDefault();
+
+    $.ajax({
+      type: "GET",
+      url: "/paintings"
+    }).done((data) => {
+      console.log('show paintings')
+      showPainting(data);
+    });
+  });
+
+  // show the Create Artist form
+  $('#addArtist').click((e) => {
+    e.preventDefault();
+
+    $.ajax({
+
+    }).done((data) => {
+      console.log('show artist form');
+      showArtistForm(data);
+    });
+  });
+
+  // show the Create Artist form
+  $('#addPainting').click((e) => {
+    e.preventDefault();
+
+    $.ajax({
+
+    }).done((data) => {
+      console.log('show artist form');
+      showPaintingForm(data);
+    });
+  });
+
+// =================================================================
+// Render templates ================================================
+// =================================================================
+
+  let showArtist = (data) => {
+    resetView();
+    
+    let $results = $('.results-div');
+    let compiledTemplate = renderTemplate_show_artists(data);
+    $results.html('').append(compiledTemplate)
   }
+
+  let showPainting = (data) => {
+    resetView();
+
+    let $results = $('.results-div');
+    let compiledTemplate = renderTemplate_show_paintings(data);
+    $results.html('').append(compiledTemplate);
+  }
+
+  let showArtistForm = (data) => {
+    resetView();
+
+    let $form = $('.form-div');
+    let compiledTemplate = renderTemplate_show_artists_form(data);
+    $form.html('').append(compiledTemplate);
+  }
+
+  let showPaintingForm = (data) => {
+    resetView();
+
+    let $form = $('.form-div');
+    let compiledTemplate = renderTemplate_show_paintings_form(data);
+    $form.html('').append(compiledTemplate);
+  }
+
+  // clear out the previous results and forms field
+  let resetView = () => {
+    $('.results-div').empty();
+    $('.form-div').empty();
+  }
+
+}
 
 
 //pulls new artist info from front end and sends to server.
